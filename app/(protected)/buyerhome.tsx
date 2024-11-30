7f99bc6b663ca4fb10c76f43b78969f3729a73cd
gemrey13 <gemreyranola@gmail.com>
2024-11-30 10:52:20 +0800

    copra home

--- app/(protected)/buyerhome.tsx
+++ app/(protected)/buyerhome.tsx
@@ -15,6 +15,8 @@ const CopraHome = () => {
     });
 
     const [totalTransaction, setTotalTransaction] = useState<number>(0);
+    const [averagePrice, setAveragePrice] = useState<number>(0);
+    const [dailyMarketPrice, setDailyMarketPrice] = useState<[]>([]);
     const [refreshing, setRefreshing] = useState(false);
 
     const [chartSummaryData, setChartSummaryData] = useState({
@@ -25,10 +27,10 @@ const CopraHome = () => {
     });
 
     const onRefresh = () => {
-        console.log("Manual refresh triggered");
         setRefreshing(true);
         fetchRevenueData();
         fetchTotalTrasaction();
+        fetchDailyMarketPrice();
         setRefreshing(false);
     };
 
@@ -36,8 +38,6 @@ const CopraHome = () => {
         try {
             const response = await axios.get("/dashboard/coprahome");
             const { revenue } = response.data;
-            console.log("revenue", revenue);
-
             setChartData({
                 expense_revenue: {
                     labels: revenue.labels,
@@ -66,41 +66,23 @@ const CopraHome = () => {
         }
     };
 
+    const fetchDailyMarketPrice = async () => {
+        try {
+            const response = await axios.get("/dashboard/coprahome/daily-market-price");
+            const { prices, averageDailyPrice } = response.data;
+            console.log("averageDailyPrice, ", averageDailyPrice);
+            setDailyMarketPrice(prices);
+            setAveragePrice(averageDailyPrice);
+        } catch (error) {
+            console.error("Error fetching revenue data:", error);
+        }
+    };
+
     useEffect(() => {
         fetchRevenueData();
         fetchTotalTrasaction();
+        fetchDailyMarketPrice();
     }, []);
-
-    const priceData = [
-        {
-            id: "1",
-            millName: "JTP Oil Mill",
-            price: "48.50",
-            subPrice: "47.75",
-        },
-        {
-            id: "2",
-            millName: "Tantangan Oil Mill",
-            price: "49.25",
-            subPrice: "48.50",
-        },
-        {
-            id: "3",
-            millName: "Mindanao Oil",
-            price: "47.75",
-            subPrice: "47.00",
-        },
-        {
-            id: "4",
-            millName: "South Cotabato Mill",
-            price: "48.75",
-            subPrice: "48.00",
-        },
-    ];
-
-    const handleViewAllPrices = () => {
-        console.log("View all prices pressed");
-    };
 
     return (
         <View className="flex-1 bg-off-100">
@@ -127,7 +109,6 @@ const CopraHome = () => {
                         />
                     </View>
 
-                    {/* Chart Section */}
                     <View className="mt-4">
                         <ChartSection
                             tabs={[{ key: "expense_revenue", label: "Revenue" }]}
@@ -136,16 +117,16 @@ const CopraHome = () => {
                         />
                     </View>
 
-                    {/* Price Section */}
-                    <View className="mt-4">
-                        <PriceSection
-                            data={priceData}
-                            average="48.56"
-                            onViewAll={handleViewAllPrices}
-                        />
-                    </View>
-
-                    {/* Bottom Spacing */}
+                    {dailyMarketPrice ? (
+                        <View className="mt-4">
+                            <PriceSection
+                                data={dailyMarketPrice}
+                                average={averagePrice.toString()}
+                            />
+                        </View>
+                    ) : (
+                        "loading...."
+                    )}
                     <View className="h-4" />
                 </View>
             </ScrollView>
